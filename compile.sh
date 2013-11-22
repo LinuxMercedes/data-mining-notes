cee9d89675c1045af67f1aa694a3874c8f2bd083
LinuxMercedes <LinuxMercedes@gmail.com>
2013-11-21 23:15:34 -0600

    Compiling notecard

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 pdflatex test3.tex && scp test3.pdf cent-desk:/mnt/dfs/nmjxv3/userweb/nmjxv3/
+pdflatex test3-card.tex && scp test3-card.pdf cent-desk:/mnt/dfs/nmjxv3/userweb/nmjxv3/
+
